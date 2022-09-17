195c0fdd0b1fbb42feaf0d27b9e7c010b1bc81a2
VV0JC13CH <wojciech@bitstudio.dev>
2022-09-17 23:50:29 +0200

    add h.fish function

---
+++ functions/h.fish
@@ -0,0 +1,25 @@
+# Wrapper for "cd" command in home dir
+function h --argument-names 'path'
+    if test -n "$path"
+        if test "$argv" = "proj"
+			cd ~/Projects	
+		else if test "$argv" = "sand"
+			cd ~/Sandbox
+		else if test "$argv" = "docs"
+			cd ~/Documents
+		else if test "$argv" = "down"
+			cd ~/Downloads
+		else if test "$argv" = "pics"
+			cd ~/Pictures
+		else if test "$argv" = "vid"
+			cd ~/Videos
+		else if test "$argv" = "conf"
+			cd ~/.config
+		else
+			cd ~/$argv 
+		end
+
+    else
+        echo "Follow the white rabbit!"
+    end
+end
