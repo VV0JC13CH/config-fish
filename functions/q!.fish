8dadf738593a384adb94a09a2780484740e452ab
VV0JC13CH <wojciech@bitstudio.dev>
2023-02-03 21:03:44 +0100

    Remove usused comm

--- functions/q!.fish
+++
@@ -1,4 +0,0 @@
-# Kills all tmux sessions and alacritty when is attached to it
-function q!
-	pkill tmux
-end
--- functions/wq.fish
+++
@@ -1,4 +0,0 @@
-# Kills default os terminal, but leaves tmux running, so it's possible to return
-function wq
-    pkill x-terminal-emul
-end
