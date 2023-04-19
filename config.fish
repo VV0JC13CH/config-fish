56b45407d273500995eecef7cc0e533e61dfbeef
wbobrowski <wbobrowski@egnyte.com>
2023-04-19 14:24:58 +0200

    Add omf config to fish

--- config.fish
+++ config.fish
@@ -1,6 +1,5 @@
 fish_add_path -m ~/.local/bin
 zoxide init fish | source
-pyenv init - | source
 if status is-interactive
     # Commands to run in interactive sessions can go here
     trap _trap_exit_tmux EXIT
