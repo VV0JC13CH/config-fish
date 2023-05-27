a077744fc163843fce52726817091c8bce07b4f3
VV0JC13CH <wojciech@bitstudio.dev>
2023-05-27 23:18:47 +0200

    Add v and rg aliases

--- config.fish
+++ config.fish
@@ -1,5 +1,7 @@
 fish_add_path -m ~/.local/bin
 zoxide init fish | source
+alias v="vim"
+alias rg="rgrep"
 if status is-interactive
     # Commands to run in interactive sessions can go here
     trap _trap_exit_tmux EXIT
