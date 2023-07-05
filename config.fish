150cc6da5ad72e9fcbbc3b985ea49ee9eaa3e0f5
wbobrowski <wbobrowski@egnyte.com>
2023-07-05 16:43:12 +0200

    kubectl alias (k)

--- config.fish
+++ config.fish
@@ -2,6 +2,7 @@ fish_add_path -m ~/.local/bin
 fish_add_path -m ~/go/bin
 zoxide init fish | source
 alias v="vim"
+alias k="kubectl"
 alias rg="rgrep"
 if status is-interactive
     # Commands to run in interactive sessions can go here
