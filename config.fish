0a6da41ee8fb648b132c510ecde34451be6a95e6
VV0JC13CH <wojciech@bitstudio.dev>
2024-10-29 17:31:22 +0100

    fix ssh-add, remember passphrases after restart

--- config.fish
+++ config.fish
@@ -7,6 +7,10 @@ alias k="kubectl"
 alias rg="rgrep"
 alias lg="lazygit"
 alias leetcode="nvim leetcode.nvim"
+# Start gnome-keyring as the SSH agent
+if not pgrep -u (id -u) gnome-keyring-daemon >/dev/null
+   eval (keychain --eval --agents ssh -Q --quiet id_rsa --nogui)
+end
 if status is-interactive
     # Commands to run in interactive sessions can go here
     trap _trap_exit_tmux EXIT
